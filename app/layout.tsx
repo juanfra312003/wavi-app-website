1f0e2c0f3603b00217969cdb601e1882a180e291
Juan Francisco Ramírez Escobar <juanframireze@javeriana.edu.co>
2026-02-09 19:25:19 -0500

    feat: add web site icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,30 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: 'Wavi - Colombia',
-  description: 'Landing web site with important information about Wavi enterprise in Colombia',
-}
+  title: "Wavi - Colombia",
+  description:
+    "Landing web site with important information about Wavi enterprise in Colombia",
+
+  icons: {
+    icon: "/Logos wavi/Logo-sin-fondo-letra-azul.png", 
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="es">
       <body>
         <Header />
-        <main className="min-h-screen pt-16">
-          {children}
-        </main>
+        <main className="min-h-screen pt-16">{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
